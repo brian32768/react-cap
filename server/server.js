bafa90973ebb4dff20b83fa7c8cdcb5235772721
Brian H Wilson <brian@wildsong.biz>
2020-04-08 10:46:52 -0700

    added mysql keep alive code

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const mysql = require('mysql');
 const config = require('./config')
 const app = express()
 const port = 3000
@@ -11,41 +12,66 @@ app.use(function (req, res, next) {
     next();
 });
 
-const dsn = `mysql://${config.USER}:${config.PASSWORD}@${config.HOST}/${config.DBNAME}`
-//console.log(dsn);
+const db_config = {
+    host: config.HOST,
+    user: config.USER,
+    password: config.PASSWORD,
+    database: config.DBNAME
+}
 
 const select_inventory = "SELECT * FROM inventory"
 const select_tests = "SELECT * FROM test_results"
 
+let connection;
+
+// My database connection was dropping about every 60 seconds
+// causing server.js to terminate. So now I watch for that and
+// reconnect. Many thanks to the answer here 
+// https://stackoverflow.com/questions/20210522/nodejs-mysql-error-connection-lost-the-server-closed-the-connection
+
+function handleDisconnect() {
+
+    // Create a new connection
+    connection = mysql.createConnection(db_config);
+    connection.connect(function(err) {
+        if (err) {
+            console.error('db: Error connecting.', err);
+            setTimeout(handleDisconnect, 2000)
+        } else {
+            console.log("db: ready");
+        }
+    })
+
+    // Add a handler to reconnect whenever the connection is lost.
+    // Usually the connection drops because the mysql timeout is short;
+    // it can also drop if the mysql server is restarted.
+    connection.on('error', function(err) {
+        console.error('db: error.', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect();
+        } else {
+            throw err;
+        }
+    });
+}
+
+
 app.get('/', (req, res) => res.send(`Hello, ${config.USER}`))
 app.all('/ping', (req, res) => res.send(new Date()))
 
-const mysql = require('mysql');
-const db = mysql.createConnection(dsn);
-
 app.get('/inventory', async (req, res) => {
-    db.query(select_inventory, (err, pages) => {
+    connection.query(select_inventory, (err, pages) => {
         (err) ? res.send(err) : res.json(pages);
     })
 })
 
 app.get('/tests', async (req, res) => {
-    db.query(select_tests, (err, pages) => {
+    connection.query(select_tests, (err, pages) => {
         (err) ? res.send(err) : res.json(pages);
     })
 })
 
-// force connection to confirm settings are correct
-db.connect((err) => {
-    if (err) throw err;
-    //console.log("db: ready");
-    // ...
-    // the rest of our initialization code
-    // ...
-    app.listen(port, () => {
-        console.log(`Server listening on port ${port}`);
-    });
+handleDisconnect();
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
 });
-
-
-
